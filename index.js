7112a36e537e0988980e3822c4a4a21cdd4765b6
Sawee Rawal <saweerawal1998@gmail.com>
2021-09-26 22:24:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var dob         = document.querySelector("#dob");
+var luckyNumber = document.querySelector("#lucky-number");
+var checkbtn    = document.querySelector("#check-btn");
+var result      = document.querySelector("#result");
+
+function calculateSum(dateOfBirth)
+{
+   var sum=0;
+   for(var index in dateOfBirth){
+           sum = sum + Number(dateOfBirth[index]);
+   }
+   return sum;
+}
+
+function checkBirthdayLucky()
+{
+    var dateOfBirth = dob.value;
+    var dateOfBirth = dateOfBirth.replaceAll("-","");
+    var sum = calculateSum(dateOfBirth);
+    var MyLuckyNumber = Number(luckyNumber.value);
+
+    if(MyLuckyNumber == ""){
+        result.innerText="Enter a number"; 
+    }
+    else{
+        if(sum%MyLuckyNumber===0){
+            result.innerText="lucky number is lucky 🤩";
+        }
+        else{
+            result.innerText="lucky number is not so lucky 😟";
+        }
+    }
+
+}
+
+
+checkbtn.addEventListener("click", checkBirthdayLucky);
+
+
